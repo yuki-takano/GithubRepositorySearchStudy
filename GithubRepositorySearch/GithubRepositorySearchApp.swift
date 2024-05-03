d6f9f87e7e56ad0e9328a8abec35922eed01ddee
takanoyuki <pmapers@gmail.com>
2024-05-03 23:53:48 +0900

    空プロジェクトを作成

---
+++ GithubRepositorySearch/GithubRepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositorySearchApp.swift
+//  GithubRepositorySearch
+//
+//  Created by takanoyuki on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

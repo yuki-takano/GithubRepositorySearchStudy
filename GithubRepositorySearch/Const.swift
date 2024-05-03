198e2f8cf8fd8f1340322a7fba9760957d558397
takanoyuki <pmapers@gmail.com>
2024-05-04 00:34:26 +0900

    - アプリで使う定数を定義

---
+++ GithubRepositorySearch/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  GithubRepositorySearch
+//
+//  Created by takanoyuki on 2024/05/04.
+//
+
+import Foundation
+
+struct Const {
+    static let token: String = "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"
+    static let url: URL = URL(string: "https://api.github.com/graphql")!
+}

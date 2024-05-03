681a3316314179988e25f6d8d8c6155ff8f7e2cb
takanoyuki <pmapers@gmail.com>
2024-05-04 00:27:29 +0900

    - リポジトリの検索一覧で使うVIewを仮で定義 - デフォルトのContentVIewを削除して上記のViewを指定

---
+++ RepositoryListView.swift
@@ -0,0 +1,21 @@
+//
+//  RepositoryListView.swift
+//  GithubRepositorySearch
+//
+//  Created by takanoyuki on 2024/05/04.
+//
+
+import SwiftUI
+import Combine
+
+struct RepositoryListView: View {
+
+    var body: some View {
+        Text("test")
+    }
+}
+
+#Preview {
+    RepositoryListView()
+}
+

b50deff992bc2b51e95f7582d9be0f5965042e4f
takanoyuki <pmapers@gmail.com>
2024-05-04 00:34:41 +0900

    - Constに関するコメントを定義

--- GithubRepositorySearch/Const.swift
+++ GithubRepositorySearch/Const.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct Const {
+    // 自身のトークンを設定して下さい。
     static let token: String = "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"
     static let url: URL = URL(string: "https://api.github.com/graphql")!
 }
